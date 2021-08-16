59fe5b4099d2cb07c4297453be3149f4ecfa439d
zul-adn <zbojetx@gmail.com>
2021-08-16 14:30:42 +0700

    Add threejs

--- src/App.js
+++ src/App.js
@@ -1,206 +1,92 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 
-// import './css/glide.core.min.css';
-import './css/main.css';
-import './css/tes.css';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
-import Slider from "react-slick";
+import "./css/styles.css";
+import "./css/styles2.css";
 
-
-
-const App = () => {
-
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
-  };
-
+function Cylinder() {
+  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(TextureLoader, [
+    'https://i.gifer.com/ycY.gif',
+    'https://37.media.tumblr.com/f174f6ccfa4ed07692ab38c7a31d5864/tumblr_n69cl3N9Sv1roeqdqo1_500.gif'
+  ])
   return (
-    <section>
-      <nav>
-        <h5>Logo.</h5>
-        <ul>
-          <li>Home</li>
-          <li>Collections</li>
-          <li>Blog</li>
-          <li>Contact</li>
-        </ul>
-        <div className="search">
-          <i className="fa fa-search"></i>
-        </div>
-      </nav>
+    <mesh
+      position={[0, 0, 0]}
+      rotation={[0, 0, 0]}
+    >
+      <cylinderBufferGeometry attach="geometry" args={[3, 3, 2, 100]} />
+      <meshBasicMaterial
+        displacementScale={0.2}
+        map={colorMap}
+        displacementMap={displacementMap}
+        normalMap={normalMap}
+        roughnessMap={roughnessMap}
+        aoMap={aoMap}
+      />
+    </mesh>
+  );
+}
 
-      <div className="content">
-        <div className="side-indicator">
-          <div className="line"></div>
-          <div className="index one">
-            <p>1</p>
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <div className="navbar-wrapper">
+        <nav className="navbar">
+          <div>
+            <img src="https://placeholder.com/wp-content/uploads/2018/10/placeholder-1.png" className="logo" />
           </div>
-          <div className="index two">
-            <p>2</p>
+          <div>
+            <ul className="list">
+              <li><a>Home</a></li>
+              <li><a>Project</a></li>
+              <li><a>Portofolio</a></li>
+              <li><a>Abous Us</a></li>
+            </ul>
           </div>
-          <div className="index three">
-            <p>3</p>
+        </nav>
+      </div>
+  
+      <Canvas
+        pixelRatio={window.devicePixelRatio}
+        camera={{
+          position: [3, 0, 3.8]
+        }}
+        style={{ position: "absolute" }}
+        >
+        <Suspense fallback={null}>
+          <Physics>
+            <Cylinder />
+          </Physics>
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
+
+      <div class="footer-wrapper">
+        <div class="footer">
+          <div class="page">
+            <span id="page-number"></span>
           </div>
-          <div className="index four">
-            <p>4</p>
+          <div class="footer-desc">
+
           </div>
-        </div>
-        <div className="text-wrapper">
-          <div className="text">
-            <div id="title">
-              {/* <h4>INDONESIA</h4> */}
-              {/* <h4>KERALA</h4>
-              <h4>BALI</h4>
-              <h4>THAILAND</h4> */}
-            </div>
-            {/* <div id="description">
-              <p>
-                As the larget archipelagic country in the world, Indonesia is
-                blessed with so many different people, cultures, customs,
-                traditions, artworks, food, animals, plants, landscapes, and
-                everything that made it almost like 100 (or even 200)countries
-                melted beautifully into one.
-              </p>
-              <p>
-                Kerala, a state on India's tropical Malabar Coast, has nearly
-                600km of Arabian Sea shoreline. It's known for its palm-lined
-                beaches and backwaters, a network of canals. Inland are the
-                Western Ghats, mountains whose slopes support tea, coffee and
-                spice plantations as well as wildlife.
-              </p>
-              <p>
-                Bali is an Indonesian island known for its forested volcanic
-                mountains, iconic rice paddies, beaches and coral reefs. The
-                island is home to religious sites such as cliffside Uluwatu
-                Temple. To the south, the beachside city of Kuta has lively
-                bars, while Seminyak, Sanur and Nusa Dua are popular resort
-                towns.
-              </p>
-              <p>
-                Thailand is a Southeast Asian country. It's known for tropical
-                beaches, opulent royal palaces, ancient ruins and ornate temples
-                displaying figures of Buddha. In Bangkok, the capital, an
-                ultramodern cityscape rises next to quiet canalside communities
-                and the iconic temples of Wat Arun, Wat Pho and the Emerald
-                Buddha Temple (Wat Phra Kaew).
-              </p>
-            </div> */}
-            {/* <div className="btn">
-              <p>Explore</p>
-              <i className="fa fa-arrow-right"></i>
-            </div> */}
-          </div>
-        </div>
-        <div>
-
-            <div class="container">
-              <div class="carousel">
-                <div class="item a"><img src="https://i.pinimg.com/originals/bb/89/ee/bb89ee0b2434ec247081cc1458abba83.gif" /></div>
-                <div class="item b"><img src="https://uploads.toptal.io/blog/image/124464/toptal-blog-image-1508845777150-847ff1a13b29d4b832694404acb67f7a.gif" /></div>
-                <div class="item c"><img src="https://static.wixstatic.com/media/ade75f_75a585aa1797487ca840645e0784988f~mv2.gif" /></div>
-                <div class="item d"><img src="https://norwegianscitechnews.com/wp-content/uploads/2019/11/vr.gif" /></div>
-                <div class="item e"><img src="https://i.gifer.com/GgWw.gif" /></div>
-                <div class="item f"><img src="https://images.prismic.io/love-website-2020/8bfd9ebb-1980-4404-b1d2-310288df5806_gif6.gif" /></div>
-              </div>
-            </div>
-
-          {/* <div className="glide"> */}
-          {/* <Slider {...settings}>
-              <div>
-                <div >
-                  <img src="https://i.pinimg.com/originals/bb/89/ee/bb89ee0b2434ec247081cc1458abba83.gif" />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <img src="https://uploads.toptal.io/blog/image/124464/toptal-blog-image-1508845777150-847ff1a13b29d4b832694404acb67f7a.gif" />
-                </div>
-              </div>
-              <div>
-                <div >
-                  <img src="https://i.pinimg.com/originals/bb/89/ee/bb89ee0b2434ec247081cc1458abba83.gif" />
-                </div>
-              </div>
-              <div>
-                <div >
-                  <img src="https://i.pinimg.com/originals/bb/89/ee/bb89ee0b2434ec247081cc1458abba83.gif" />
-                </div>
-              </div>
-              <div>
-              </div>
-            </Slider> */}
-          {/* </div> */}
-
-          {/* <div className="glide">
-            <div className="glide_cover"></div>
-            <div className="glide__track" data-glide-el="track">
-              <ul className="glide__slides">
-                <li className="glide__slide">
-                  <div className="slide one">
-
-                    <div className="slider-image">
-                      <img src="https://i.pinimg.com/originals/bb/89/ee/bb89ee0b2434ec247081cc1458abba83.gif" />
-                    </div>
-                  </div>
-                </li>
-                <li className="glide__slide">
-                  <div className="slide two">
-                    <div className="slider-image">
-                      <img src="https://uploads.toptal.io/blog/image/124464/toptal-blog-image-1508845777150-847ff1a13b29d4b832694404acb67f7a.gif" />
-                    </div>
-                  </div>
-                </li>
-                <li className="glide__slide">
-                  <div className="slide three">
-                    <div className="slider-image">
-                      <img src="https://static.wixstatic.com/media/ade75f_75a585aa1797487ca840645e0784988f~mv2.gif" />
-                    </div>
-                  </div>
-                </li>
-                <li className="glide__slide">
-                  <div className="slide four">
-                    <div className="slider-image">
-                      <img src="https://norwegianscitechnews.com/wp-content/uploads/2019/11/vr.gif" />
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div data-glide-el="controls" className="controls">
-              <div data-glide-dir="<" id="prev">
-                <i className="fa fa-arrow-left"></i>
-              </div>
-              <div data-glide-dir=">" id="next">
-                <i className="fa fa-arrow-right"></i>
-              </div>
-            </div>
-          </div> */}
-
         </div>
       </div>
-      <div className="background"></div>
-    </section>
-    // <div>
-    //   <h2>Center Mode</h2>
-    //   <div className="container">
-    //     {/* <div className="carousel"> */}
-    //       <Slider {...settings}>
-    //         <div class="item a">a</div>
-    //         <div class="item b">b</div>
-    //         <div class="item c ">c</div>
-    //         <div class="item d">d</div>
-    //         <div class="item e ">E</div>
-    //         <div class="item f">F</div>
-    //       </Slider>
-    //     {/* </div> */}
-    //   </div>
-    // </div>
+    </>
   );
-};
-
-export default App;
+}
